d7a068e3bf7ec3ed0442dec889e02c363a9bfa96
Lucas M. Lara <LucasML@Live.com>
2024-12-11 14:57:10 -0300

    adicionar configuração de autenticação com NextAuth e provedor de credenciais

---
+++ auth.ts
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import db from "./lib/db";
+import { compareSync } from "bcrypt-ts";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [Credentials({
+    credentials: {
+      email: { label: "Email", type: "email" },
+      password: {  label: "Password", type: "password" },
+    },
+    async authorize({password, email})  {
+      if(!email || !password) {
+        return null;
+      }
+
+      const user = await db.user.findUnique({
+        where: {
+          email: email as string,
+        },
+      })
+      if(!user) {
+        return null;
+      }
+
+      const matches = compareSync(password as string, user.password ?? '');
+      if(matches) {
+        return user;
+      }
+      return null;
+    }
+  })],
+});
+
+
+
