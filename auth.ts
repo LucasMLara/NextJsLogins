576eccd6d77bb395a9f52cbdc5acc7c76fbc213e
Lucas M. Lara <LucasML@Live.com>
2024-12-12 21:22:27 -0300

    adicionar suporte ao Prisma Adapter e configurar estratégia de sessão JWT

--- auth.ts
+++ auth.ts
@@ -3,6 +3,10 @@ import Credentials from "next-auth/providers/credentials";
 import db from "./lib/db";
 import { compareSync } from "bcrypt-ts";
 import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const {
   handlers: { GET, POST },
@@ -10,13 +14,17 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy :'jwt',
+  },
   providers: [Credentials({
     credentials: {
       email: { label: "Email", type: "email" },
-      password: {  label: "Password", type: "password" },
+      password: { label: "Password", type: "password" },
     },
-    async authorize({password, email})  {
-      if(!email || !password) {
+    async authorize({ password, email }) {
+      if (!email || !password) {
         return null;
       }
 
@@ -25,18 +33,18 @@ export const {
           email: email as string,
         },
       })
-      if(!user) {
+      if (!user) {
         return null;
       }
 
       const matches = compareSync(password as string, user.password ?? '');
-      if(matches) {
+      if (matches) {
         return user;
       }
       return null;
     }
   }),
-GitHub({})],
+  GitHub({})],
 });
 
 
